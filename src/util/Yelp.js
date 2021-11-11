9961e3a71d2e97c03b5826bc5049dc3c6a1c6c59
Yuliya Barysevich <47508299+YuliyaBarysevich@users.noreply.github.com>
2021-11-11 17:20:42 -0500

    trying to deploy

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@
 
 const Yelp = {
   search(term, location, sortBy){
-    return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
       }
